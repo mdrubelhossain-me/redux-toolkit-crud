ca515673902896309dc9501c41beb399422fe407
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-05-08 17:00:57 +0600

    bootstrap installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const App = () => {
   return (
     <div>
       <h1>Hello, World!</h1>
+      <button className="btn btn-primary">Click Me</button>
     </div>
   );
 };
